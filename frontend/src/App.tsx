0644fb40258fd87e846b2c5acca8b99e56849fb9
swatimahadevan <swasvem@gmail.com>
2025-02-09 01:45:55 +0800

    added-layout-landing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css'
 import {PublicClientApplication} from "@azure/msal-browser";
 import {MsalProvider} from "@azure/msal-react";
-import MainContent from "./MainContent.tsx";
+// import MainContent from "./MainContent.tsx";
+import PageLayout from './components/PageLayout.tsx';
+import LandingPage from './pages/Landing.tsx';
+
 
 interface AppProps {
     instance: PublicClientApplication
 }
+
 const App = ({instance} : AppProps) =>{
     return (
         <MsalProvider instance={instance}>
-            {/*<PageLayout>*/}
-                <MainContent/>
-            {/*</PageLayout>*/}
+            <PageLayout>
+                <LandingPage/>
+                {/* <MainContent/> */}
+            </PageLayout>
         </MsalProvider>
     )
 }
