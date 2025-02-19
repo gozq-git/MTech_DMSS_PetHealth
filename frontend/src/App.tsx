777e6fd33252c90ecb8d71ca341f12b390dbca13
xueyangp <67683433+xueyangp@users.noreply.github.com>
2025-02-19 23:05:47 +0800

    added snackbar provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
 import './App.css';
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PageLayout from './components/PageLayout.tsx';
-import LandingPage from './pages/Landing.tsx';
-import PetPage from './pages/PetPage.tsx';
+import {PublicClientApplication} from "@azure/msal-browser";
+import {MsalProvider} from "@azure/msal-react";
+import MainContent from "./MainContent.tsx";
+import {SnackbarProvider} from "./providers/SnackbarProvider.tsx";
 
 interface AppProps {
     instance: PublicClientApplication;
 }
 
-const App = ({ instance }: AppProps) => {
+const App = ({instance}: AppProps) => {
     return (
         <MsalProvider instance={instance}>
-            <Router>
-                <PageLayout>
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/pets" element={<PetPage />} />
-                    </Routes>
-                </PageLayout>
-            </Router>
+            <SnackbarProvider>
+                <MainContent/>
+            </SnackbarProvider>
         </MsalProvider>
     );
 };
