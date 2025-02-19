32e846691b545eb010e6a6abacf839e37f7e44b1
xueyangp <67683433+xueyangp@users.noreply.github.com>
2025-02-20 00:26:00 +0800

    reorganised side bar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -3,62 +3,106 @@ import Toolbar from '@mui/material/Toolbar';
 import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import PetsIcon from '@mui/icons-material/Pets';
 import IconButton from '@mui/material/IconButton';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import {useMsal} from "@azure/msal-react";
+import {Button} from "@mui/material";
+import {loginRequest} from "../authConfig.ts";
+import {AutoAwesome, Home, MedicalServices, SvgIconComponent} from "@mui/icons-material";
+import {SideBarButton} from "./SideBarButton.tsx";
 
 const drawerWidth = 240;
 
 interface SideBarProps {
-  isOpen: boolean;
-  onClose: () => void;
+    isOpen: boolean;
+    onClose: () => void;
 }
 
-const SideBar = ({ isOpen, onClose }: SideBarProps) => {
-  const navigate = useNavigate();
+export interface SideBarItem {
+    id: String,
+    text: String,
+    description: String,
+    icon: SvgIconComponent,
+    endpoint: String
+}
 
-  return (
-    <Drawer
-      variant="temporary"
-      open={isOpen}
-      onClose={onClose}
-      sx={{ '& .MuiDrawer-paper': { width: drawerWidth } }}
-    >
-      <Toolbar>
-        <IconButton onClick={onClose}>
-          <ChevronLeftIcon />
-        </IconButton>
-      </Toolbar>
-      <Divider />
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton onClick={() => navigate('/pets')}>
-            <ListItemIcon>
-              <PetsIcon />
-            </ListItemIcon>
-            <ListItemText primary="My Pets" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
-  );
+const sideBarItems: SideBarItem[] = [
+    {
+        id: 'home',
+        text: 'Home',
+        description: 'Navigate to Home',
+        icon: Home,
+        endpoint: '/home'
+    },
+    {
+        id: 'pets',
+        text: 'My Pets',
+        description: 'View my Pets',
+        icon: PetsIcon,
+        endpoint: '/pets'
+    },
+    {
+        id: 'healthcare',
+        text: 'Healthcare',
+        description: 'Medical and Healthcare Services',
+        icon: MedicalServices,
+        endpoint: '/healthcare'
+    },
+    {
+        id: 'services',
+        text: 'Other Services',
+        description: 'Other Services',
+        icon: AutoAwesome,
+        endpoint: '/services'
+    }
+];
+
+const SideBar = ({isOpen, onClose}: SideBarProps) => {
+    const navigate = useNavigate();
+    const handleNavigation = (endpoint: String) => {
+        navigate(endpoint.toString()); // Convert String to string if needed
+    }
+    const {instance} = useMsal();
+    const activeAccount = instance.getActiveAccount();
+    const handleLoginRedirect = () => {
+        instance.loginRedirect(loginRequest).catch((error) => console.log(error));
+    };
+    return (
+        <Drawer
+            variant="temporary"
+            open={isOpen}
+            onClose={onClose}
+            sx={{'& .MuiDrawer-paper': {width: drawerWidth}}}
+        >
+            <Toolbar>
+                <IconButton onClick={onClose}>
+                    <ChevronLeftIcon/>
+                </IconButton>
+            </Toolbar>
+            <Divider/>
+            {
+                activeAccount ?
+                    <List>
+                        {sideBarItems.map((item) => (
+                            <ListItem key={String(item.id)} disablePadding>
+                                <SideBarButton
+                                    item={item}
+                                    onClick={handleNavigation}
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                    :
+                    <Button onClick={handleLoginRedirect}>Login to Access Features</Button>
+            }
+            <Divider/>
+            <List>
+
+            </List>
+        </Drawer>
+    );
 };
 
 export default SideBar;
