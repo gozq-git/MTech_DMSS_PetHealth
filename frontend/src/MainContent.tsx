e0a9e888e0813d94e464032174a271c0393c5ea5
xueyangp <67683433+xueyangp@users.noreply.github.com>
2025-02-20 00:25:41 +0800

    refactored the router routes

--- frontend/src/MainContent.tsx
+++ frontend/src/MainContent.tsx
@@ -1,8 +1,16 @@
 import {AuthenticatedTemplate, UnauthenticatedTemplate, useMsal} from "@azure/msal-react";
 import {loginRequest} from "./authConfig.ts";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LandingPage from "./pages/Landing.tsx";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import PetPage from "./pages/PetPage.tsx";
+import PageLayout from "./components/PageLayout.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import HealthcarePage from "./pages/HealthcarePage.tsx";
+import ServicesPage from "./pages/ServicesPage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+import SettingsPage from "./pages/SettingsPage.tsx";
+import {AuthenticatedRoutes, UnauthenticatedRoutes} from "./routes/AppRouter.tsx";
 
 const MainContent = () => {
     /**
@@ -10,7 +18,7 @@ const MainContent = () => {
      * that tells you what msal is currently doing. For more, visit:
      * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
      */
-    const { instance } = useMsal();
+    const {instance} = useMsal();
     const activeAccount = instance.getActiveAccount();
 
     const handleLoginRedirect = () => {
@@ -30,36 +38,17 @@ const MainContent = () => {
             .catch((error) => console.log(error));
     };
     return (
-        <div className="App">
-            <AuthenticatedTemplate>
-                {activeAccount ? (
-                    <>
-                        <div>
-                            <a href="https://vite.dev" target="_blank">
-                                <img src={viteLogo} className="logo" alt="Vite logo" />
-                            </a>
-                            <a href="https://react.dev" target="_blank">
-                                <img src={reactLogo} className="logo react" alt="React logo" />
-                            </a>
-                        </div>
-                        <h1>Vite + React</h1>
-                        <div className="card">
-                            <button type="button" onClick={handleLogoutRedirect}>
-                                Log out
-                            </button>
-                        </div>
-                    </>
-                ) : null}
-            </AuthenticatedTemplate>
-            <UnauthenticatedTemplate>
-                <button type="button" onClick={handleSignUpRedirect}>
-                    Sign up
-                </button>
-                <button type="button" onClick={handleLoginRedirect}>
-                    Sign In
-                </button>
-            </UnauthenticatedTemplate>
-        </div>
+        <BrowserRouter>
+            <PageLayout>
+                <AuthenticatedTemplate>
+                    <AuthenticatedRoutes />
+                </AuthenticatedTemplate>
+
+                <UnauthenticatedTemplate>
+                    <UnauthenticatedRoutes />
+                </UnauthenticatedTemplate>
+            </PageLayout>
+        </BrowserRouter>
     );
 };
 
