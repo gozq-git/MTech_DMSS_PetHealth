4387d2a5b3a9093101b766b65e5f3bfd079afec1
xueyangp <67683433+xueyangp@users.noreply.github.com>
2025-02-20 00:24:50 +0800

    added a "Routes" const object

---
+++ frontend/src/routes/routes.ts
@@ -0,0 +1,30 @@
+export const ROUTES = {
+    LANDINGS: {
+        path: '/',
+        label: 'Landing'
+    },
+    HOME: {
+        path: '/home',
+        label: 'Home'
+    },
+    PETS: {
+        path: '/pets',
+        label: 'Pets'
+    },
+    HEALTHCARE: {
+        path: '/healthcare',
+        label: 'Healthcare'
+    },
+    SERVICES: {
+        path: '/services',
+        label: 'Services'
+    },
+    PROFILE: {
+        path: '/profile',
+        label: 'Profile'
+    },
+    SETTINGS: {
+        path: '/settings',
+        label: 'Settings'
+    }
+} as const;
