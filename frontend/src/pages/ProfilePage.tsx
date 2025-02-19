5d12b819c497cbd659e3b457c86923a22310b2d6
xueyangp <67683433+xueyangp@users.noreply.github.com>
2025-02-20 00:24:35 +0800

    added placeholder page

---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,8 @@
+import {UserProfileForm} from "./UserProfileForm.tsx";
+
+const ProfilePage = () => {
+    return (<UserProfileForm/>)
+
+}
+
+export default ProfilePage;
---
+++ frontend/src/pages/ServicesPage.tsx
@@ -0,0 +1,13 @@
+import {Container} from "@mui/material";
+import Box from "@mui/material/Box";
+
+const ServicesPage = () => {
+    return (<Container maxWidth="lg">
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+            This is the Services page.
+        </Box>
+    </Container>)
+
+}
+
+export default ServicesPage;
