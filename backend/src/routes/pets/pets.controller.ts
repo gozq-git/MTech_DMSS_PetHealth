69b4e70167b60affec866c9d7e7be08d77e7d423
Venkat Krishna <venkatachalamks.96@gmail.com>
2025-02-24 23:33:00 +0800

    pets model insert,retrieve and get by owner id

--- backend/src/routes/pets/pets.controller.ts
+++ backend/src/routes/pets/pets.controller.ts
@@ -1,23 +1,40 @@
 import PetsService from "./pets.service";
 
-
 const PetsController = {
   getPets: async () => {
     try {
       const pets = await PetsService.getPets();
       return pets;
     } catch (error) {
-      console.log(error);
-      throw new Error("Error retrieving Pets");
+      console.error(error);
+      throw new Error("Error retrieving pets");
     }
   },
-  retrievePet: async (ID: string) => {
+  retrievePet: async (id: string) => {
     try {
-      const pets = await PetsService.retrievePet(ID);
+      const pet = await PetsService.retrievePet(id);
+      return pet;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error retrieving pet");
+    }
+  },
+  getPetsByOwner: async (ownerId: string) => {
+    try {
+      const pets = await PetsService.getPetsByOwner(ownerId);
       return pets;
     } catch (error) {
-      console.log(error);
-      throw new Error("Error retrieving user");
+      console.error(error);
+      throw new Error("Error retrieving pets by owner ID");
+    }
+  },
+  insertPet: async (petData: any) => {
+    try {
+      const newPet = await PetsService.insertPet(petData);
+      return newPet;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error inserting pet");
     }
   },
 };
