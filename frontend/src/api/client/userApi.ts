b365fe3cae51e035a61c57f15682a9a0ccb00b67
swatimahadevan <swasvem@gmail.com>
2025-03-15 11:27:36 +0800

    reg-logic
    if user not registered, direct to profile page

--- frontend/src/api/client/userApi.ts
+++ frontend/src/api/client/userApi.ts
@@ -4,7 +4,7 @@ import {User} from "../types/user.ts";
 
 export interface UserApi {
     getUsers: () => Promise<User[]>;
-    retrieveUser: (userId: string) => Promise<User>;
+    retrieveUser: () => Promise<User[]>;
     registerUser: (userData: Omit<User, 'id'>) => Promise<User>;
     updateUser: (userId: string, userData: Partial<User>) => Promise<User>;
 }
@@ -12,7 +12,7 @@ export interface UserApi {
 export const createUserApiClient = (baseClient: BaseApiClient): UserApi => {
     return {
         getUsers: () => baseClient.get<User[]>('/api/users/getUsers'),
-        retrieveUser: (userId) => baseClient.get<User>(`/api/retrieveUser/${userId}`),
+        retrieveUser: () => baseClient.get<User[]>(`/api/users/retrieveUser`),
         registerUser: (userData) => baseClient.post<User>('/api/users', userData),
         updateUser: (userId, userData) => baseClient.put<User>(`/api/users/${userId}`, userData)
     };
