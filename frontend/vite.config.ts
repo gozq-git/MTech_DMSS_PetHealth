c9a659056646b3ccefa8758e6c99fe1c34445006
gerardozq <32777327+gerardozq@users.noreply.github.com>
2025-03-16 12:15:23 +0800

    edit vite host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     watch:{
       usePolling: true,
     },
-    allowedHosts: ['pethealthplatform-dev.azurewebsites.net']
+    allowedHosts: ['pethealthplatform-uat.azurewebsites.net']
   },
   plugins: [react()],
 })
