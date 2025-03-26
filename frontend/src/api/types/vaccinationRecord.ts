0c0a84f51acf6f6060dc10059e7352c423e2cc65
xueyangp <67683433+xueyangp@users.noreply.github.com>
2025-03-26 15:10:05 +0800

    interface for add vaccination request

--- frontend/src/api/types/vaccinationRecord.ts
+++ frontend/src/api/types/vaccinationRecord.ts
@@ -1,12 +1,24 @@
-export default interface VaccinationRecord {
+export interface VaccinationRecord {
     id: string;
     petId: string;
     name: string;
     description: string;
     administeredDate: string;
     administeredBy: string;
+    expiryDate: string;
     nextDueDate: string;
+    lotNumber: string;
     isValid: boolean;
     created: string;
     updated: string;
+}
+export interface VaccinationRecordRequest {
+    name: string;
+    description: string;
+    administered_at: string;
+    administered_by: string;
+    expires_at: string;
+    lot_number: string;
+    next_due_at: string;
+    is_valid: boolean;
 }
